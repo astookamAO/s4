f0ced91fb81110a9fa30cac9ff02aba544326116
Assetou Kamagate <assetou@humongous.io>
2023-11-12 23:47:47 -0800

    Includes string

--- index4.js
+++ index4.js
@@ -1,27 +1,26 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 4000;
 
-const arg =  (req, response) => {
+const arg = (req, response) => {
 
-let greeting = ""
+  let greeting = "";
 
-  if(req.url.includes('fr')){
-  greeting = "Bonjour"
+  if (req.url.includes("abab") || req.url.includes("ousmane")) {
+    greeting = "Hello World";
+    response.statusCode = 200;
+  } else {
+    greeting = "Not found";
+    response.statusCode = 404;
   }
 
-  if(req.url.includes('en')){
-    greeting = "Hello"
-    }
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    response.end(`${greeting}${req.url}`);
+  response.setHeader("Content-Type", "text/plain");
+  response.end(`${greeting} ${req.url.replace(/\//,'')}`);
 };
 
 const server = http.createServer(arg);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
-}); 
+});
